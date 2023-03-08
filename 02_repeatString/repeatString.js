7bfa8529a86b686b7ee2a88a47c38c51211dd14b
Jenn Bushey <117766897+jennbushey@users.noreply.github.com>
2023-03-08 16:25:53 -0700

    Updated with if statement in loop. Passed fifth test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -2,12 +2,16 @@ let string;
 let num;
 
 const repeatString = function(string,num) {
-    let solution = "";
+    if(num<0){
+        return 'ERROR';
+    } else {
+        let solution = "";
     for(let i=1;i<=num;i++){
         solution = solution + string;
         //console.log(solution,num,i);
     }
     return solution;
+    }
 }
 // Do not edit below this line
 module.exports = repeatString;
