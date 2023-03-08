f86a33c2db9674f1494b2c8870e49b66b59f2f62
Jenn Bushey <117766897+jennbushey@users.noreply.github.com>
2023-03-08 16:21:33 -0700

    Loop passes first test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
+let string;
+let num;
 
-};
-
+const repeatString = function(string,num) {
+    let solution = "";
+    for(let i=1;i<=num;i++){
+        solution = solution + string;
+        //console.log(solution,num,i);
+    }
+    return solution;
+}
 // Do not edit below this line
 module.exports = repeatString;
