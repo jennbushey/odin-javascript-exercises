197546c12b9e63b4b656858df1aab9e22c832b84
Jenn Bushey <117766897+jennbushey@users.noreply.github.com>
2023-03-09 09:49:45 -0700

    Function passed first test.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
-
+let userArray= [];
+let userRemove;
+const removeFromArray = function(userArray, userRemove) {
+let newArray =[];
+for (let userContent of userArray){
+    if (userContent === userRemove) {
+        continue;
+    }
+    else {
+        newArray.push(userContent);
+        }
+    //console.log(userArray, userRemove);
+    }
+    //console.log(newArray);
+    return newArray;
 };
 
 // Do not edit below this line
