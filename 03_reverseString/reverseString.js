5530c349921162571492a73149ce8b26c4168299
Jenn Bushey <117766897+jennbushey@users.noreply.github.com>
2023-03-08 17:00:03 -0700

    Function passes the first test

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,26 @@
-const reverseString = function() {
+let userInput;
+const reverseString = function(userInput) {
+    let i = 0;
+    let initialArray = [];
+    while (i < userInput.length){ //converts userInput to array
+        initialArray[i] = userInput.slice(i,i+1);
+        i++;
+    }
+    //console.log(initialArray);
 
+    let secondArray = []; //creates a new array in reverse order from initalArray
+    let j = 0; 
+    while(j < userInput.length){
+        i--;
+        secondArray[j] = initialArray[i];
+        j++;
+    }
+    //console.log(secondArray);
+    
+    let functionOutput;
+    functionOutput = secondArray.toString(); //converts array to string
+
+    return functionOutput.replace(/\,/g,'');//returns string without commas
 };
 
 // Do not edit below this line
