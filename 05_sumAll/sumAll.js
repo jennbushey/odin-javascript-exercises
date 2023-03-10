afc663502365b9d72dc340e6c203130f3d67ba1f
Jenn Bushey <117766897+jennbushey@users.noreply.github.com>
2023-03-09 21:35:59 -0700

    Function passes all tests. Didn't use the right integer test according to the solution.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,9 +1,22 @@
 const sumAll = function(a,b) {
+    
+    if (typeof a !== "number" || typeof b !== "number") return "ERROR"; //my answer. Doesn't handle decimals.
+    //if (!Number.isInteger(a) || !Number.isInteger(b)) return "ERROR"; //solution. Where does number.isinteger come from? ALSO doesn't handle decimals.
+
+    if (a < 0 || b < 0)  return "ERROR";
+   
+    if (a > b) {
+        let j = b;
+        let k = a;
+        a = j;
+        b = k;
+    } 
+     
     let c = 0;
-  
     for (;a<=b;a++){
-        c += a;
+            c += a;
     }
+
     return c;
 };
 
