bb4f37c8662ac3328073f298a81d3fed93b540eb
Jenn Bushey <117766897+jennbushey@users.noreply.github.com>
2023-03-10 14:57:05 -0700

    Revised function to be one line. Didn't know return stuff is the same as function {} return stuff. Sneaky readme.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,12 +1,6 @@
 const leapYears = function(year) {
-   // if ((year % 4 === 0) || 
     
-    if ((year % 100 === 0) && (year % 400 === 0)) {
-        return true;
-    } else if (!(year % 100 === 0 ) && (year % 4 === 0)) {
-        return true;
-    } 
-    else return false;
+    return ((year % 100 === 0 && year % 400 === 0) || year % 100 !== 0 && year % 4 === 0);
 };
 
 // Do not edit below this line
