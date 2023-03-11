b305cfbe49691aba3939abcafade5ada5ba9f920
Jenn Bushey <117766897+jennbushey@users.noreply.github.com>
2023-03-10 19:10:48 -0700

    I think I did the first exercise correctly?

---
+++ DOM_exercises/exercise.js
@@ -0,0 +1,30 @@
+// your JavaScript file
+const container = document.querySelector('#container');
+
+const content = document.createElement('div');
+content.classList.add('content');
+content.textContent = 'This is the glorious text-content!';
+
+container.appendChild(content);
+
+const red = document.createElement('p');
+red.classList.add('red');
+red.style.color = 'red';
+red.textContent = "Hey I'm red!"
+container.appendChild(red);
+
+const head3 = document.createElement('h3');
+head3.classList.add('blue');
+head3.style.color = 'blue';
+head3.textContent = "I'm a blue h3!"
+container.appendChild(head3);
+
+const div = document.createElement('div');
+div.classList.add('div');
+const head1 = document.createElement('h1');
+const p2 = document.createElement('p');
+head1.textContent = "I'm in a div"
+p2.textContent = "ME TOO!"
+div.appendChild(head1);
+div.appendChild(p2);
+container.appendChild(div);
