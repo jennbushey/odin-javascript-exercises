7c61d381787775f9c907e91251e5b244598ff9ee
Jenn Bushey <117766897+jennbushey@users.noreply.github.com>
2023-03-21 23:18:31 -0600

    1/6 totally focused on reversing the string rather than returning true/false. Copied web answer.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,13 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    // convert to lowercase
+    // remove non letter characters
+    // reverse
+    // join with no spaces or commas
+    // return string == reversed string
+
+    const first = string.toLowerCase().match(/[a-z0-9]/g).join('');
+    const reversedString = string.toLowerCase().match(/[a-z0-9]/g).reverse().join('');
+    return first == reversedString;
 
 };
 
