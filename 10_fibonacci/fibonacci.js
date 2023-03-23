ecb7b1d5cf399e0c3eb1821a496426793e0ef8f1
Jenn Bushey <117766897+jennbushey@users.noreply.github.com>
2023-03-22 19:10:55 -0600

    Solved the problem.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function (value) {
+    // first solution
+    let array = [1, 1];
+    if (value < 0) return "OOPS"
 
+    for (i = 0; i <= value; i++) {
+        array.push(array[i] + array[i + 1]);
+    }
+    console.log(array);
+    return array[value - 1];
 };
 
 // Do not edit below this line
