c0a1577789bc790068eb9c22bf79b3c439a189df
Jenn Bushey <117766897+jennbushey@users.noreply.github.com>
2023-03-22 19:16:11 -0600

    Solved using array methods instead of using temp a and b variables. My solution is shorter than the odin solution.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -2,12 +2,14 @@ const fibonacci = function (value) {
     // first solution
     let array = [1, 1];
     if (value < 0) return "OOPS"
+    if (count === 0) return 0; //didn't have a 0 case
 
     for (i = 0; i <= value; i++) {
         array.push(array[i] + array[i + 1]);
     }
     console.log(array);
     return array[value - 1];
+
 };
 
 // Do not edit below this line
